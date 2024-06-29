ea0a2a3620120af7d2854292e8ca2aff856e7a63
colli <colli@DESKTOP-GEIRPDO>
2024-06-29 17:18:28 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppTemplate
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
